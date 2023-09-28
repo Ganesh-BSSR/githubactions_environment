4893ae78dea06d5aed13ba0161d6886cff8be809
Ganesh-BSSR <145440864+Ganesh-BSSR@users.noreply.github.com>
2023-09-29 03:22:59 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 
-app.listen(process.env.PORT, () => {
-  console.log(`Database name: ${process.env.DATABASE_NAME}`)
-  console.log(`Database username: ${process.env.DATABASE_USERNAME}`)
-  console.log(`Database password: ${process.env.DATABASE_PASSWORD}`)
-  console.log('Server started at port: ' + process.env.PORT)
-})
+// app.listen(process.env.PORT, () => {
+//   console.log(`Database name: ${process.env.DATABASE_NAME}`)
+//   console.log(`Database username: ${process.env.DATABASE_USERNAME}`)
+//   console.log(`Database password: ${process.env.DATABASE_PASSWORD}`)
+//   console.log('Server started at port: ' + process.env.PORT)
+// })
+console.log(`Database name: ${process.env.DATABASE_NAME}`)
+console.log(`Database username: ${process.env.DATABASE_USERNAME}`)
+console.log(`Database password: ${process.env.DATABASE_PASSWORD}`)
+console.log('Server started at port: ' + process.env.PORT)
